8f838c0ba2f0c3cbe714d781ccd47ea3a68a7e78
toanth <tobias.theuer@gmx.de>
2025-06-08 14:57:34 +0200

    StaticVector actually works like a `std::vector`, so elements with size <= index <capacity aren't being constructed. (#48)
    Co-authored-by: 87 <dev@87flowers.com>

--- src/util/static_vector.hpp
+++ src/util/static_vector.hpp
@@ -1,112 +1,221 @@
 #pragma once
 
+#include "util/types.hpp"
+
 #include <algorithm>
 #include <array>
 #include <cassert>
+#include <memory>
 #include <utility>
-
-
-#include "util/types.hpp"
 
 namespace Clockwork {
 
+
+/// C++26 has `std::inplace_vector`, but that's not yet implemented in any major stdlib.
+/// This template is a simplified version of that idea.
+/// We don't need to worry about special cases like elements being (partially) `const`, etc.
+/// We also don't care about exception safety and simply assume that copying/moving `T` doesn't throw.
 template<typename T, usize cap>
 class StaticVector {
+
 public:
     using value_type     = T;
     using iterator       = T*;
     using const_iterator = const T*;
     using size_type      = usize;
 
-    constexpr StaticVector() = default;
-    ~StaticVector()          = default;
-
-    constexpr StaticVector(const StaticVector& other) :
-        len(other.len) {
-        std::copy(other.begin(), other.end(), begin());
+    StaticVector() :
+        m_len(0) {
+        // don't zero `m_storage`
+    }
+    ~StaticVector() {
+        clear();
     }
 
-    constexpr StaticVector(StaticVector&& other) :
-        len(std::exchange(other.len, 0)) {
-        std::move(other.begin(), other.end(), begin());
+    StaticVector(std::initializer_list<T> list) :
+        m_len(list.size()) {
+        assert(m_len <= cap);
+        std::uninitialized_copy(list.begin(), list.end(), data());
     }
 
-    constexpr StaticVector& operator=(const StaticVector& other) {
-        len = other.len;
-        std::copy(other.begin(), other.end(), begin());
+    explicit StaticVector(usize len) :
+        m_len(len) {
+        std::uninitialized_value_construct_n(data(), len);
+    }
+
+    StaticVector(const StaticVector& other) :
+        m_len(other.m_len) {
+        std::uninitialized_copy(other.begin(), other.end(), data());
+    }
+
+    StaticVector(StaticVector&& other) noexcept :
+        m_len(other.m_len) {
+        std::uninitialized_move(other.begin(), other.end(), data());
+        // technically, this isn't necessary, but it should help catch potential bugs.
+        other.clear();
+    }
+
+    StaticVector& operator=(const StaticVector& other) {
+        if (other.m_len > m_len) {
+            std::copy_n(other.begin(), m_len, data());
+            std::uninitialized_copy(other.begin() + m_len, other.end(), end());
+            m_len = other.m_len;
+        } else {
+            std::copy(other.begin(), other.end(), data());
+            resize(other.m_len);
+        }
         return *this;
     }
 
-    constexpr StaticVector& operator=(StaticVector&& other) {
-        len = std::exchange(other.len, 0);
-        std::move(other.begin(), other.end(), begin());
+    StaticVector& operator=(StaticVector&& other) noexcept {
+        if (&other == this) {
+            return *this;
+        }
+        if (other.m_len > m_len) {
+            std::move(other.begin(), other.begin() + m_len, data());
+            std::uninitialized_move(other.begin() + m_len, other.end(), end());
+            m_len = other.m_len;
+        } else {
+            std::move(other.begin(), other.end(), data());
+            resize(other.m_len);
+        }
+        other.clear();
         return *this;
     }
 
-    constexpr iterator push_back(const T& value) {
-        assert(len < cap);
-        data[len] = value;
-        return &data[len++];
+    template<typename... Args>
+        requires(std::constructible_from<T, Args...>)
+    iterator emplace_back(Args&&... args) {
+        assert(m_len < cap);
+        T* res = std::construct_at(data() + m_len, std::forward<Args>(args)...);
+        m_len++;
+        return res;
     }
 
-    constexpr void append(StaticVector&& other) {
-        assert(len + other.len < cap);
-        std::move(other.begin(), other.end(), end());
-        len += std::exchange(other.len, 0);
+    iterator push_back(const T& value) {
+        assert(m_len < cap);
+        T* res = std::construct_at(data() + m_len, value);
+        m_len++;
+        return res;
     }
 
-    constexpr void clear() {
-        len = 0;
+    iterator push_back(T&& value) {
+        return emplace_back(std::move(value));
     }
 
-    [[nodiscard]] constexpr usize size() const {
-        return len;
+    void append(StaticVector&& other) {
+        assert(m_len + other.m_len <= cap);
+        std::uninitialized_move(other.begin(), other.end(), end());
+        m_len += other.m_len;
+        other.clear();
     }
-    [[nodiscard]] constexpr usize capacity() const {
+
+    T pop_back() {
+        T& last = back();
+        T  res  = std::move(last);
+        std::destroy_at(&last);
+        m_len--;
+        return res;
+    }
+
+    void clear() {
+        std::destroy_n(data(), m_len);
+        m_len = 0;
+    }
+
+    [[nodiscard]] usize size() const {
+        return m_len;
+    }
+    [[nodiscard]] usize capacity() const {
         return cap;
     }
-    [[nodiscard]] constexpr bool empty() const {
-        return len == 0;
+    [[nodiscard]] bool empty() const {
+        return m_len == 0;
     }
 
-    constexpr void resize(usize new_size) {
+    /// This value initializes elements, that is, it zeros primitive types like `int`.
+    void resize(usize new_size) {
         assert(new_size <= cap);
-        len = new_size;
+        if (new_size <= m_len) {
+            std::destroy(begin() + new_size, end());
+        } else {
+            std::uninitialized_value_construct(end(), begin() + new_size);
+        }
+        m_len = new_size;
     }
 
-    constexpr T& operator[](usize index) {
-        assert(index < len);
-        return data[index];
-    }
-    constexpr const T& operator[](usize index) const {
-        assert(index < len);
-        return data[index];
+    void resize(usize new_size, const T& value) {
+        assert(new_size <= cap);
+        if (new_size <= m_len) {
+            std::destroy(begin() + new_size, end());
+        } else {
+            std::uninitialized_fill(end(), begin() + new_size, value);
+        }
+        m_len = new_size;
     }
 
-    [[nodiscard]] constexpr iterator begin() {
-        return &data[0];
+    T& operator[](usize index) {
+        assert(index < m_len);
+        return data()[index];
     }
-    [[nodiscard]] constexpr const_iterator begin() const {
-        return &data[0];
+    const T& operator[](usize index) const {
+        assert(index < m_len);
+        return data()[index];
     }
-    [[nodiscard]] constexpr const_iterator cbegin() const {
+
+    // We never access the raw bytes directly except to immediately call `std::construct_at`.
+    // This means we don't need to call `std::launder`.
+    // Unfortunately, the reinterpret_cast means this can't be constexpr.
+    [[nodiscard]] T* data() {
+        return reinterpret_cast<T*>(m_storage.data());
+    }
+
+    [[nodiscard]] const T* data() const {
+        return reinterpret_cast<const T*>(m_storage.data());
+    }
+
+    [[nodiscard]] iterator begin() {
+        return data();
+    }
+    [[nodiscard]] const_iterator begin() const {
+        return data();
+    }
+    [[nodiscard]] const_iterator cbegin() const {
         return begin();
     }
 
-    [[nodiscard]] constexpr iterator end() {
-        return &data[len];
+    [[nodiscard]] iterator end() {
+        return begin() + m_len;
     }
-    [[nodiscard]] constexpr const_iterator end() const {
-        return &data[len];
+    [[nodiscard]] const_iterator end() const {
+        return begin() + m_len;
     }
-    [[nodiscard]] constexpr const_iterator cend() const {
+    [[nodiscard]] const_iterator cend() const {
         return end();
     }
 
-protected:
-    usize len = 0;
-    // TODO: std::array default initializes, and default initialization is a slowdown. We should REALLY remove this and let initialization be handled manually.
-    std::array<T, cap> data;
+    T& back() {
+        assert(m_len > 0);
+        return data()[m_len - 1];
+    }
+    const T& back() const {
+        assert(m_len > 0);
+        return data()[m_len - 1];
+    }
+
+    friend auto operator<=>(const StaticVector& lhs, const StaticVector& rhs) {
+        return std::lexicographical_compare_three_way(lhs.begin(), lhs.end(), rhs.begin(),
+                                                      rhs.end());
+    }
+
+    friend auto operator==(const StaticVector& lhs, const StaticVector& rhs) {
+        return std::equal(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
+    }
+
+private:
+    alignas(T) std::array<std::byte, cap * sizeof(T)> m_storage;
+
+    usize m_len;
 };
 
 }  // namespace Clockwork
---
+++ tests/test_static_vector.cpp
@@ -0,0 +1,153 @@
+#include "util/static_vector.hpp"
+
+#include <algorithm>
+#include <cstdlib>  // for std::exit
+#include <iostream>
+#include <source_location>
+
+#define REQUIRE(x) \
+    do { \
+        if (!(x)) { \
+            std::cout << "Assertion failed at line " << std::source_location::current().line() \
+                      << ": " #x; \
+            std::exit(1); \
+        } \
+    } while (false)
+
+using namespace Clockwork;
+
+
+static isize ctr = 0;
+
+struct TestType {
+    isize x;
+    TestType() :
+        x(0) {
+        ctr += 1;
+    }
+
+    TestType(const TestType& other) :
+        x(other.x) {
+        ctr += 1;
+    }
+    ~TestType() {
+        ctr -= 1;
+    }
+
+    explicit TestType(isize x) :
+        x(x) {
+        ctr += 1;
+    }
+
+    TestType& operator=(const TestType& other) = default;
+
+    friend bool operator==(const TestType& lhs, const TestType& rhs) = default;
+};
+
+struct OnlyMoves {
+    OnlyMoves()                                = default;
+    OnlyMoves(const OnlyMoves&)                = delete;
+    OnlyMoves(OnlyMoves&&) noexcept            = default;
+    OnlyMoves& operator=(const OnlyMoves&)     = delete;
+    OnlyMoves& operator=(OnlyMoves&&) noexcept = default;
+};
+
+void push_pop_back() {
+    REQUIRE(ctr == 0);
+    StaticVector<TestType, 42> vec;
+    REQUIRE(vec.size() == 0);
+    REQUIRE(vec.capacity() == 42);
+    REQUIRE(ctr == 0);
+    vec.push_back(TestType{100});
+    REQUIRE(vec.size() == 1);
+    REQUIRE(ctr == 1);
+    REQUIRE(vec.back().x == 100);
+    vec.pop_back();
+    REQUIRE(vec.empty());
+    REQUIRE(ctr == 0);
+}
+
+void special_member_funs() {
+    REQUIRE(ctr == 0);
+    StaticVector<TestType, 10> vec;
+    vec.push_back(TestType(0));
+    vec.emplace_back(1);
+    vec.push_back(TestType(2));
+    REQUIRE(vec.size() == 3);
+    REQUIRE(ctr == 3);
+    {
+        auto copy = vec;
+        REQUIRE(ctr == 6);
+        REQUIRE(copy == vec);
+        copy = copy;             // NOLINT
+        copy = std::move(copy);  // NOLINT
+        REQUIRE(ctr == 6);
+        REQUIRE(copy.end() - copy.begin() == 3);
+        auto copy2 = std::move(copy);
+        REQUIRE(copy.empty());
+        REQUIRE(vec == copy2);
+        REQUIRE(ctr == 6);
+        copy = std::move(copy2);
+        REQUIRE(ctr == 6);
+        copy2 = copy;
+        REQUIRE(ctr == 9);
+        REQUIRE(vec.size() == copy2.size());
+        for (usize i = 0; i < vec.size(); ++i) {
+            REQUIRE(vec[i] == copy2[i]);
+        }
+    }
+    REQUIRE(ctr == 3);
+    auto vec2 = vec;
+    vec2.clear();
+    vec2.emplace_back(1);
+    vec = vec2;
+    REQUIRE(vec == vec2);
+    vec2.resize(4, TestType{100});
+    vec = vec2;
+    REQUIRE(vec.size() == 4);
+}
+
+void misc() {
+    REQUIRE(ctr == 0);
+    StaticVector<TestType, 11> vec{TestType(1), TestType(2), TestType(3)};
+    REQUIRE(vec.size() == 3);
+    vec.emplace_back(4);
+    REQUIRE(ctr == 4);
+    vec.resize(6);
+    REQUIRE(ctr == 6);
+    REQUIRE(vec.back().x == 0);
+    auto vec2 = vec;
+    vec2.pop_back();
+    vec.append(std::move(vec2));
+    REQUIRE(ctr == 11);
+    REQUIRE(vec.back().x == 0);
+    vec.resize(10, TestType{-1});
+    REQUIRE(vec.back().x == 4);
+    REQUIRE(vec[2].x == 3);
+    vec.clear();
+    REQUIRE(ctr == 0);
+}
+
+// this mostly tests that the following doesn't attempt to copy an element.
+void no_copies() {
+    StaticVector<OnlyMoves, 3> vec;
+    vec.push_back(OnlyMoves());
+    vec.emplace_back(OnlyMoves());
+    vec.emplace_back();
+    auto vec2 = std::move(vec);
+    vec       = std::move(vec2);
+    REQUIRE(vec.size() == 3);
+    vec.resize(0);
+    REQUIRE(vec.empty());
+}
+
+static_assert(std::ranges::contiguous_range<StaticVector<int, 42>>);
+
+int main() {
+    push_pop_back();
+    special_member_funs();
+    misc();
+    no_copies();
+    REQUIRE(ctr == 0);
+    return 0;
+}
