6bf993bec75bf4bcb96f0e4446fff908f9714e5f
Eduardo Cáceres <eduherminio@users.noreply.github.com>
2025-06-13 14:11:06 +0200

    Adjust static eval with TT score (#87)
    bench 8198719

--- src/search.cpp
+++ src/search.cpp
@@ -182,8 +182,15 @@ Value Worker::search(Position& pos, Stack* ss, Value alpha, Value beta, Depth de
     bool  is_in_check = pos.is_in_check();
     Value static_eval = is_in_check ? -VALUE_INF : evaluate(pos);
 
-    if (!ROOT_NODE && !is_in_check && depth <= 6 && static_eval >= beta + 80 * depth) {
-        return static_eval;
+    // Reuse TT score as a better positional evaluation
+    auto tt_adjusted_eval = static_eval;
+    if (tt_data
+        && tt_data->bound != (tt_data->score > static_eval ? Bound::Upper : Bound::Lower)) {
+        tt_adjusted_eval = tt_data->score;
+    }
+
+    if (!ROOT_NODE && !is_in_check && depth <= 6 && tt_adjusted_eval >= beta + 80 * depth) {
+        return tt_adjusted_eval;
     }
 
     if (!ROOT_NODE && !is_in_check && depth >= 3) {
