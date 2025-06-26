b63c7326b7beb905e4946a2e53afae9181d1f33a
87 <dev@87flowers.com>
2025-06-26 18:09:15 +0000

    Use std::clamp (#102)
    No functional change.
    Bench: 7573112

--- src/search.cpp
+++ src/search.cpp
@@ -241,7 +241,7 @@ Value Worker::search(Position& pos, Stack* ss, Value alpha, Value beta, Depth de
             i32 reduction =
               static_cast<i32>(0.77 + std::log(depth) * std::log(moves_played) / 2.36);
             reduction -= PV_NODE;
-            Depth reduced_depth = std::min(std::max(new_depth - reduction, 1), new_depth);
+            Depth reduced_depth = std::clamp<Depth>(new_depth - reduction, 1, new_depth);
             value = -search<false>(pos_after, ss + 1, -alpha - 1, -alpha, reduced_depth, ply + 1);
             if (value > alpha && reduced_depth < new_depth) {
                 value = -search<false>(pos_after, ss + 1, -alpha - 1, -alpha, new_depth, ply + 1);
