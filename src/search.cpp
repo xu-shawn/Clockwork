7fcfed69c786c911ed481179e6d0193708009216
Eren Araz <77514455+ProgramciDusunur@users.noreply.github.com>
2025-07-20 19:33:01 +0300

    Add PVS SEE Pruning (#114)
    Elo | 24.12 +- 10.42 (95%)
    SPRT | 8.0+0.08s Threads=1 Hash=16MB
    LLR | 3.01 (-2.94, 2.94) [0.00, 5.00]
    Games | N: 2496 W: 915 L: 742 D: 839
    Penta | [80, 259, 461, 304, 144]
    https://clockworkopenbench.pythonanywhere.com/test/115/
    
    Bench: 1497824

--- src/search.cpp
+++ src/search.cpp
@@ -255,6 +255,12 @@ Value Worker::search(Position& pos, Stack* ss, Value alpha, Value beta, Depth de
             }
         }
 
+        Value see_threshold = quiet ? -67 * depth : -32 * depth;
+        // SEE PVS Pruning
+        if (depth <= 10 && !ROOT_NODE && !SEE::see(pos, m, see_threshold)) {
+            continue;
+        }
+
         // Do move
         Position pos_after = pos.move(m);
         moves_played++;
