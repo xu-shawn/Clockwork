c031926cb104a70aaf63209b35abf34d4ba6b63e
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-08-28 15:33:59 +0200

    Also check for threats on from square for history indexing (#139)
    Test  | double_hist_threats
    Elo   | 10.49 +- 6.38 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=16MB
    LLR   | 2.97 (-2.94, 2.94) [0.00, 5.00]
    Games | N: 5966 W: 1893 L: 1713 D: 2360
    Penta | [173, 699, 1139, 719, 253]
    
    Bench: 1583988

--- src/history.hpp
+++ src/history.hpp
@@ -14,15 +14,18 @@ public:
     History() = default;
 
     i32 get_quiet_stats(const Position& pos, Move move) const {
-        auto to_attacked = pos.attack_table(~pos.active_color()).read(move.to()) != 0;
-        return m_main_hist[static_cast<usize>(pos.active_color())][move.from_to()][to_attacked];
+        auto to_attacked   = pos.attack_table(~pos.active_color()).read(move.to()) != 0;
+        auto from_attacked = pos.attack_table(~pos.active_color()).read(move.from()) != 0;
+        return m_main_hist[static_cast<usize>(pos.active_color())][move.from_to()]
+                          [from_attacked * 2 + to_attacked];
     }
 
     void update_quiet_stats(const Position& pos, Move move, i32 bonus) {
-        auto to_attacked = pos.attack_table(~pos.active_color()).read(move.to()) != 0;
-        update_hist_entry(
-          m_main_hist[static_cast<usize>(pos.active_color())][move.from_to()][to_attacked],
-          bonus);
+        auto to_attacked   = pos.attack_table(~pos.active_color()).read(move.to()) != 0;
+        auto from_attacked = pos.attack_table(~pos.active_color()).read(move.from()) != 0;
+        update_hist_entry(m_main_hist[static_cast<usize>(pos.active_color())][move.from_to()]
+                                     [from_attacked * 2 + to_attacked],
+                          bonus);
     }
 
     void clear() {
