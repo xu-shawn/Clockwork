2c608a359d0008fb1f3ff4730b607b40f9233848
Shawn Xu <xu107288696@gmail.com>
2025-10-07 16:27:12 -0700

    secret persy draw tech
    bench 16773119

--- src/common.hpp
+++ src/common.hpp
@@ -11,7 +11,7 @@ constexpr i32   MAX_PLY     = 256;
 constexpr Value VALUE_INF   = 32501;
 constexpr Value VALUE_MATED = 32500;
 constexpr Value VALUE_WIN   = 32000;
-constexpr Value VALUE_DRAW  = 0;
+constexpr Value VALUE_DRAW  = 60;
 
 enum class Color {
     White,
