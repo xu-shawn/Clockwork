0d4f78bbdf9c59540df380a7399208b84340c14a
Eren Araz <77514455+ProgramciDusunur@users.noreply.github.com>
2025-10-07 16:05:49 +0300

    Introduce Multicut  (#273)
    To be tweaked probably by Dr. Extensiom himself.
    
    ```
    Test  | multicut
    Elo   | 2.70 +- 2.17 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=16MB
    LLR   | 2.97 (-2.94, 2.94) [0.00, 5.00]
    Games | N: 31932 W: 7418 L: 7170 D: 17344
    Penta | [316, 3755, 7601, 3953, 341]
    ```
    https://clockworkopenbench.pythonanywhere.com/test/670/
    
    Bench: 17351435

--- src/search.cpp
+++ src/search.cpp
@@ -529,6 +529,11 @@ Value Worker::search(
                 }
             }
 
+            // Multicut
+            else if (singular_beta >= beta) {
+                return singular_beta;
+            }            
+            
             // Negative Extensions
             else if (tt_data->score >= beta) {
                 extension = -1;
