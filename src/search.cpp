741aa0414b00b6aa065341e7f743cc49e712fdb2
87 <dev@87flowers.com>
2025-11-04 00:11:29 +0000

    Fix -VALUE_INF from cached evaluation (#314)
    This was the issue in the position `4k3/6rP/8/5K2/8/8/8/8 w - - 0 68`.
    
    There was a TT entry collision, and the TT entry eval was from a
    position that was in check. This meant that the TT entry had an eval
    value of `-VALUE_INF`.
    
    This patch calls `evaluate` for any TT entry that has an out-of-range
    evaluation. We also clamp evals to ensure this isn't accidentally
    triggered by unclamped evaluations.
    
    Passed STC
    ```
    Test  | fix-tt-eval
    Elo   | 0.94 +- 2.73 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=16MB
    LLR   | 2.98 (-2.94, 2.94) [-4.50, 0.50]
    Games | N: 19518 W: 4817 L: 4764 D: 9937
    Penta | [198, 2236, 4840, 2285, 200]
    ```
    https://clockworkopenbench.pythonanywhere.com/test/891/
    
    Passed LTC
    ```
    Test  | fix-tt-eval
    Elo   | 0.22 +- 2.09 (95%)
    SPRT  | 40.0+0.40s Threads=1 Hash=64MB
    LLR   | 2.97 (-2.94, 2.94) [-4.50, 0.50]
    Games | N: 29582 W: 6743 L: 6724 D: 16115
    Penta | [180, 3403, 7593, 3448, 167]
    ```
    https://clockworkopenbench.pythonanywhere.com/test/884/
    
    Bench: 11533740

--- src/search.cpp
+++ src/search.cpp
@@ -428,7 +428,7 @@ Value Worker::search(
     ss->static_eval   = -VALUE_INF;
     if (!is_in_check) {
         correction      = m_td.history.get_correction(pos);
-        raw_eval        = tt_data ? tt_data->eval : evaluate(pos);
+        raw_eval        = tt_data && !is_mate_score(tt_data->eval) ? tt_data->eval : evaluate(pos);
         ss->static_eval = raw_eval + correction;
         improving = (ss - 2)->static_eval != -VALUE_INF && ss->static_eval > (ss - 2)->static_eval;
 
@@ -843,7 +843,7 @@ Value Worker::quiesce(const Position& pos, Stack* ss, Value alpha, Value beta, i
     Value static_eval = -VALUE_INF;
     if (!is_in_check) {
         correction  = m_td.history.get_correction(pos);
-        raw_eval    = tt_data ? tt_data->eval : evaluate(pos);
+        raw_eval    = tt_data && !is_mate_score(tt_data->eval) ? tt_data->eval : evaluate(pos);
         static_eval = raw_eval + correction;
 
         if (!tt_data) {
@@ -930,7 +930,9 @@ Value Worker::quiesce(const Position& pos, Stack* ss, Value alpha, Value beta, i
 
 Value Worker::evaluate(const Position& pos) {
 #ifndef EVAL_TUNING
-    return static_cast<Value>(Clockwork::evaluate_stm_pov(pos, m_td.psqt_states.back()));
+    return std::clamp<Value>(
+      static_cast<Value>(Clockwork::evaluate_stm_pov(pos, m_td.psqt_states.back())), -VALUE_WIN + 1,
+      VALUE_WIN - 1);
 #else
     return -VALUE_INF;  // Not implemented in tune mode
 #endif
